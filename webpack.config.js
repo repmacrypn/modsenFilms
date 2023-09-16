67765cfc39503dfd8be15ff54c2838f856bfb3c7
repmacrypn <artik3267@gmail.com>
2023-09-16 16:48:32 +0300

    feat: webpack config upd + installed styled components/gh-pages + added declare.d.ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,54 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
-
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.tsx'),
   devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'build')
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'ts-loader'],
+        use: ['babel-loader', 'ts-loader']
       },
-    ],
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images'
+            }
+          }
+        ]
+      }
+    ]
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'build'),
+      directory: path.join(__dirname, 'build')
     },
-    port: 3000,
+    port: 3000
   },
   resolve: {
     plugins: [
       new TsconfigPathsPlugin({
-        configFile: path.join(__dirname, './tsconfig.json'),
-      }),
+        configFile: path.join(__dirname, './tsconfig.json')
+      })
     ],
     alias: {
-      '@/*': path.resolve(__dirname, 'src'),
+      '@/*': path.resolve(__dirname, 'src')
     },
-    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx']
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-    }),
-  ],
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ]
 }
