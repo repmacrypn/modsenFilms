83fdee6525fb4999ff6c00044757f84c65a11d2c
repmacrypn <artik3267@gmail.com>
2023-10-29 23:47:19 +0300

    fix: cypress tests refactoring

--- cypress/e2e/searchField.cy.ts
+++ cypress/e2e/searchField.cy.ts
@@ -1,6 +1,9 @@
 import {cy} from 'local-cypress'
 
 describe('Search Field module', () => {
+  const waitSecValue = 3000
+  const inputTitle = 'whiplash'
+
   it('search field elements should be visible', () => {
     cy.visit('http://localhost:3000')
 
@@ -11,9 +14,7 @@ describe('Search Field module', () => {
   })
 
   it('input should respond correctly after typing', () => {
-    const inputValue = 'whiplash'
-
-    cy.get('[data-testid=searchInput]').type(inputValue).should('have.value', inputValue)
+    cy.get('[data-testid=searchInput]').type(inputTitle).should('have.value', inputTitle)
   })
 
   it('hint modal should appear when the input value > 0', () => {
@@ -21,18 +22,13 @@ describe('Search Field module', () => {
   })
 
   it('should open the film modal with the corresponding trailer after clicking on the exact film', () => {
-    const waitSecValue = 3000
-
     cy.get('[data-testid=hintModal]').wait(waitSecValue).first().click()
     cy.get('[data-testid=modal]').should('be.visible')
     cy.get('[data-testid=closeButton]').wait(waitSecValue).first().click()
   })
 
   it('when you click on the button, the query "search for movies by title" should go', () => {
-    const waitSecValue = 3000
-    const filmTitle = 'whiplash'
-
-    cy.get('[data-testid=searchInput]').should('have.value', filmTitle)
+    cy.get('[data-testid=searchInput]').should('have.value', inputTitle)
     cy.get('[data-testid=searchButton]').wait(waitSecValue).click()
 
     cy.get('[data-testid=filmsList]').wait(waitSecValue).should('be.visible')
